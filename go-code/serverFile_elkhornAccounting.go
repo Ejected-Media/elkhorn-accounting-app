caa99330ad79f796632aa6f7d80b8cd3adbd3526
kyle2501@gmail.com <kyle2501@gmail.com>
2024-09-06 14:42:20 -0700

    update serverFile_elkhornAccounting.go

--- go-code/serverFile_elkhornAccounting.go
+++ go-code/serverFile_elkhornAccounting.go
@@ -1,7 +1,6 @@
 // . . Elkhorn - Accounting - Website App
 
 package main
-
 // .
 
 import (
@@ -32,4 +31,75 @@ type pageNav struct {
     pageLink string
 }
 
+// . indexHandler,  ~ for Public Pages °
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+// ,  ° . +
+    if r.URL.Path != "/" {
+    	http.NotFound(w, r)
+    	return
+    }
+
+// , ° . +
+  pageTitle := "~ . Elkhorn Accounting App - // - Website App"
+  pagePath := r.URL.Path
+  // pageType := ".."
+
+
+// ,  ° . +
+pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+    //  pageList: []pageNav {
+  //        { pageTitle: "one", pageLink: "one"},
+//          { pageTitle: "two", pageLink: "two"},
+   //       { pageTitle: "three", pageLink: "three"},
+    //  },
+  	
+  }  //. .  pageData
+  
+  
+  if pagePath == "/" {
+      pageTitle = "Index Page"
+ //     pageList = pageList
+  }
+
+
+// ,  ° . +
+  pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  indexHandler
+
+
+//  .  html url routes 
+//  .  as well as terminal cli logs
+func main() {
+// ,  ° . +
+  appName := "~ . Elkhorn Accounting App - // - Website App"
+
+// ,  ° . +
+  http.HandleFunc("/", indexHandler)
+
+// . ° ~ +
+ http.HandleFunc("/action/classSchedule", actionPage)
+
+// -- -
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+    log.Printf("Loading _webapp with default port")
+  }
+  
+// ,  ° . +
+  log.Printf("_webapp is active and Listening on port %s", port)
+
+  log.Printf("// -- - %s", appName)
+  log.Printf("_webapp now loaded and running at http://localhost:%s", port)
+
+// -- - 
+  if err := http.ListenAndServe(":"+port, nil); err != nil {
+    log.Fatal("Error Starting the HTTP Server :", err)
+    return
+  }}
 
